cefc456c64002baa6a12b2987645cfa6eb180b44
sugarcookie <latasha.hussey@gmail.com>
2017-06-09 23:49:42 -0500

    modified notes on 404 page

--- wp-content/themes/accelerate-theme-child/404.php
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -3,8 +3,8 @@
  * The template for displaying 404 pages (Not Found)
  *
  * @package WordPress
- * @subpackage Twenty_Fourteen
- * @since Twenty Fourteen 1.0
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 1.0
  */
 
 get_header(); ?>
